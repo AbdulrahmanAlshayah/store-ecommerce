5bdd4845b7f2b28d9c11efbf03cb6ede2188bce1
= <=>
2022-12-24 10:18:39 +0200

    episode 9

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,10 +10,10 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "admin" middleware group. Now create something great!
 |
 */
-
+//note that the prefix is admin for all fill route
 Route::group(['namespace' => 'Dashboard','middleware' => 'auth:admin'],function (){
     Route::get('users',function (){
         return "in admin";
@@ -20,7 +21,7 @@ Route::group(['namespace' => 'Dashboard','middleware' => 'auth:admin'],function 
 });
 
 Route::group(['namespace' => 'Dashboard'],function (){
-    Route::get('login',function (){
-        return "please login";
-    })->name('admin.login');
+    Route::get('login',[LoginController::class,'login'])->name('admin.login');
+    Route::post('login',[LoginController::class,'postLogin'])->name('admin.post.login');
+
 });
