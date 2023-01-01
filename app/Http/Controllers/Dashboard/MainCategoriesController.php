86c239c46f88e2fbe71a2d0c66a6fd619d7bea90
= <=>
2023-01-01 17:21:49 +0200

    episode 039-Apply-OOP-Traits-Enumerations-and-Final-concep

--- app/Http/Controllers/Dashboard/MainCategoriesController.php
+++ app/Http/Controllers/Dashboard/MainCategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Enumerations\CategoryType;
 use App\Http\Requests\MainCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -37,7 +38,7 @@ class MainCategoriesController extends Controller
 
             //if user choose main category then we must remove paret id from the request
 
-            if ($request->type == 1) //main category
+            if ($request->type == CategoryType::mainCategory) //main category
             {
                 $request->request->add(['parent_id' => null]);
             }
@@ -50,13 +51,13 @@ class MainCategoriesController extends Controller
             $category->name = $request->name;
             $category->save();
 
+            DB::commit();
             return redirect()->route('admin.maincategories')->with(['success' => 'تم ألاضافة بنجاح']);
 
-            DB::commit();
 
         } catch (\Exception $ex) {
             DB::rollback();
-
+            return redirect()->route('admin.maincategories')->with(['error' => 'حدث خطا ما برجاء المحاوله لاحقا']);
         }
 
 
